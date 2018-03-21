60937d1679636942b353ca01664840ba3707a2a1
avijit sarkar <avisofficeone@gmail.com>
2018-03-21 16:58:56 +0530

    plugin - added entypoint infos

---
+++ inform.php
@@ -0,0 +1,45 @@
+<?php
+/*
+    Plugin Name: inform
+    Plugin URI: https://github.com/insetavijit/inform.git
+    Description: A simple Clean minimalists modern wordpress plugin development workflow - seed 
+    Version: 1.0.0
+    Author: Avijit sarkar
+    Author URI: https://github.com/insetavijit/
+    License: GPLv2 or later
+    Text Domain: inset
+*/
+
+defined( 'ABSPATH' ) or die( 'Get Out !' ) ;
+
+if(file_exists( dirname( __FILE__ ) .'/vendor/autoload.php' )){
+    require_once ( dirname( __FILE__ ) .'/vendor/autoload.php' );
+}
+
+
+define ( 'INFORM_PLUGIN_BASE_NAME' , plugin_basename( __FILE__ ));
+
+
+use modules\Base\Activate ;
+use modules\Base\Deactivate ;
+
+
+function inform_activation (){
+    Activate :: activate ();
+}
+function inform_deactivation (){
+    Deactivate :: deactivate ();
+}
+
+
+register_activation_hook    ( __FILE__ , 'inform_activation' );
+register_deactivation_hook  ( __FILE__ , 'inform_deactivation');
+
+
+
+
+if( class_exists( 'modules\\Init') ){
+    modules\Init::register_services() ;
+}else{
+    // print_r( Inc );
+}
